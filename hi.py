9fa7d1f3e0baad2972c0317db0e2ccc288d1d88a
vinit <vinitkumar93341@gmail.com>
2025-11-01 22:33:51 +0530

    virtual env took me

--- hi.py
+++ hi.py
@@ -1,1 +1,26 @@
+import sys
 print("hello world")
+
+print(sys.version)
+
+# always work in venv
+
+#linux bash 
+
+#  python -m venv venv
+# source venv/Scripts/activate
+# deactivate
+
+
+#  chai_shop/
+#       run.py -> starts the app  -> module
+#       chai.py
+#       processing/
+#       utils/                --> packages (has init.py files)
+#           __init__.py
+
+
+#pep8 -->>
+# 4 spaces, never tabs
+# chai , not c1
+# formatters
